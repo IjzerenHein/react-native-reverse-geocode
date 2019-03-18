b0dfa2dc40ca0bd745ac82e9a2bb39fb4595f856
Hein Rutjes <hrutjes@gmail.com>
2019-03-18 15:04:21 +0100

    Added threadpool to Android code

--- android/src/main/kotlin/com/kiwi/reversegeocode/RNReverseGeocodeManager.kt
+++ android/src/main/kotlin/com/kiwi/reversegeocode/RNReverseGeocodeManager.kt
@@ -1,10 +1,10 @@
-
 package com.kiwi.reversegeocode
 
 import android.location.Geocoder
 import android.location.Address
 import android.app.Activity
 import android.text.TextUtils
+import android.util.Log
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -17,11 +17,15 @@ import com.facebook.react.bridge.WritableMap
 import com.facebook.react.bridge.WritableNativeMap
 
 import java.util.Locale
+import java.util.concurrent.Executors
+import java.lang.Runnable
 
 import kotlin.collections.List
 
 
 class RNReverseGeocodeManager(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+  private val executorService = Executors.newCachedThreadPool();
 
   companion object {
     const val MAX_RESULTS = 10
@@ -83,56 +87,62 @@ class RNReverseGeocodeManager(reactContext: ReactApplicationContext) : ReactCont
       return
     }
 
-    val latitude = region.getDouble("latitude")
-    val longitude = region.getDouble("longitude")
-    val latitudeDelta = region.getDouble("latitudeDelta")
-    val longitudeDelta = region.getDouble("longitudeDelta")
+    executorService.execute(Runnable {
+      Log.d(getName(), "SearchForLocation: " + searchText + "...")
+      val latitude = region.getDouble("latitude")
+      val longitude = region.getDouble("longitude")
+      val latitudeDelta = region.getDouble("latitudeDelta")
+      val longitudeDelta = region.getDouble("longitudeDelta")
 
-    val lowerLeftLatitude = latitude - Math.abs(latitudeDelta)
-    val lowerLeftLongitude = longitude - Math.abs(longitudeDelta)
-    val upperRightLatitude = latitude + Math.abs(latitudeDelta)
-    val upperRightLongitude = longitude + Math.abs(longitudeDelta)
-    
-    try {
-      val geocoder = Geocoder(reactApplicationContext, Locale.getDefault())
-      
-      // First fetch as many local addresses as possible
-      val localAddresses = geocoder.getFromLocationName(
-        searchText,
-        MAX_RESULTS,
-        lowerLeftLatitude,
-        lowerLeftLongitude,
-        upperRightLatitude,
-        upperRightLongitude
-      )
-      var addresses = localAddresses.orEmpty()
+      val lowerLeftLatitude = latitude - Math.abs(latitudeDelta)
+      val lowerLeftLongitude = longitude - Math.abs(longitudeDelta)
+      val upperRightLatitude = latitude + Math.abs(latitudeDelta)
+      val upperRightLongitude = longitude + Math.abs(longitudeDelta)
 
-      // If less than the max-amount of items have been found,
-      // then add other more remote possibilities
-      if ((localAddresses != null) && (localAddresses.size < MAX_RESULTS)) {
-        var remoteAddresses = geocoder.getFromLocationName(
+      try {
+        val geocoder = Geocoder(reactApplicationContext, Locale.getDefault())
+
+        // First fetch as many local addresses as possible
+        Log.d(getName(), "SearchForLocation: local...")
+        val localAddresses = geocoder.getFromLocationName(
           searchText,
-          MAX_RESULTS
+          MAX_RESULTS,
+          lowerLeftLatitude,
+          lowerLeftLongitude,
+          upperRightLatitude,
+          upperRightLongitude
         )
-        if (remoteAddresses != null) {
+        var addresses = localAddresses.orEmpty()
 
-          // Filter out local-search duplicates
-          remoteAddresses = remoteAddresses.filter { remoteAddress -> localAddresses.find { localAddress -> localAddress.getLatitude() == remoteAddress.getLatitude() && localAddress.getLongitude() == remoteAddress.getLongitude() } == null };
+        // If less than the max-amount of items have been found,
+        // then add other more remote possibilities
+        if ((localAddresses != null) && (localAddresses.size < MAX_RESULTS)) {
+          Log.d(getName(), "SearchForLocation: remote...")
+          var remoteAddresses = geocoder.getFromLocationName(
+            searchText,
+            MAX_RESULTS
+          )
+          if (remoteAddresses != null) {
 
-          // Limit the remove search results to not exceed MAX_RESULTS
-          if ((remoteAddresses.size + localAddresses.size) > MAX_RESULTS) {
-            remoteAddresses = remoteAddresses.subList(0, MAX_RESULTS - localAddresses.size);
+            // Filter out local-search duplicates
+            remoteAddresses = remoteAddresses.filter { remoteAddress -> localAddresses.find { localAddress -> localAddress.getLatitude() == remoteAddress.getLatitude() && localAddress.getLongitude() == remoteAddress.getLongitude() } == null };
+
+            // Limit the remove search results to not exceed MAX_RESULTS
+            if ((remoteAddresses.size + localAddresses.size) > MAX_RESULTS) {
+              remoteAddresses = remoteAddresses.subList(0, MAX_RESULTS - localAddresses.size);
+            }
+
+            // Concatenate addresses
+            addresses = localAddresses + remoteAddresses
           }
-
-          // Concatenate addresses
-          addresses = localAddresses + remoteAddresses
         }
-      }
 
-      callback.invoke(null,formatAddresses(addresses))
-    
-    } catch (e: Exception) {
-      callback.invoke(e.message, null)
-    }
+        Log.d(getName(), "SearchForLocation, results: " + addresses.size);
+        callback.invoke(null, formatAddresses(addresses))
+        Log.d(getName(), "SearchForLocation... DONE");
+      } catch (e: Exception) {
+        callback.invoke(e.message, null)
+      }
+    });
   }
 }
